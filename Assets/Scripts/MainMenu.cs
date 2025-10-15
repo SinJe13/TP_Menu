3bc3515c7c2016414851498a10339da2f32665c0
SinJe <gaetan.margry17@gmail.com>
2025-10-15 17:38:31 +0200

    fade

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,28 @@
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using TMPro;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public Image ImageFade;
+
+    private void Start()
+    {
+        ImageFade.DOFade(0, 0); //alpha à 0
+        ImageFade.gameObject.SetActive(false); //on le désactive pour qu'il ne bloque pas les boutons
+    }
+
     public void OnClickPlay()
+    {
+        ImageFade.gameObject.SetActive(true);
+        ImageFade.DOFade(1, 0.8f).OnComplete(FadeComplete);
+    }
+
+    private void FadeComplete()
     {
         SceneManager.LoadScene("Gameplay");
     }
